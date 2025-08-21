7ddc4d1a9209a8367b4a1fd927005f11797683a2
Jakob J. Garde <kontakt@jakobgarde.dk>
2025-08-21 20:06:53 +0200

    Final prototype for exam project.
    Refactor code structure for improved readability and maintainability

--- src/Overlay/ControlSettings.tsx
+++ src/Overlay/ControlSettings.tsx
@@ -14,18 +14,24 @@ export default function ControlSettings() {
 
   return (
     <div className="flex h-fit flex-col items-center gap-2 rounded-lg border-2 border-gray-200/30 bg-gray-700/30 px-4 py-2 backdrop-blur-md [&>*]:divide-gray-900/60 [&>*]:border-gray-900/60">
-      <div className="pointer-events-auto grid cursor-pointer grid-flow-row items-center justify-items-center divide-y-2 rounded-lg border-2 text-2xl [&>*]:bg-clip-padding [&>*]:p-3">
-        <div
-          className="flex h-full items-center justify-center bg-gradient-to-tr from-gray-800/50 from-20% to-purple-700/30 to-90% transition-colors duration-300 hover:bg-gradient-to-tr hover:from-gray-900/70 hover:to-purple-700/50"
-          onClick={toggleRLEnabled}
-        >
-          {isRLEnabled ? <MdAutoAwesome /> : <FaRegKeyboard />}
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex items-center gap-2">
+          <div
+            className="pointer-events-auto flex h-full cursor-pointer items-center justify-center rounded-lg border-2 border-purple-800/30 bg-gradient-to-tr from-gray-800/50 from-20% to-purple-700/30 to-90% p-3 text-2xl transition-colors duration-300 hover:bg-gradient-to-tr hover:from-gray-900/70 hover:to-purple-700/50"
+            onClick={toggleRLEnabled}
+          >
+            {isRLEnabled ? <FaRegKeyboard /> : <MdAutoAwesome />}
+          </div>
+          <div>Toggle RL/Manual</div>
         </div>
-        <div
-          className="flex h-full items-center justify-center bg-gradient-to-tr from-gray-800/50 from-20% to-purple-700/30 to-90% transition-colors duration-300 hover:bg-gradient-to-tr hover:from-gray-900/70 hover:to-purple-700/50"
-          onClick={toggleObstaclesEnabled}
-        >
-          {isObstaclesEnabled ? <FaExplosion /> : <FaRoad />}
+        <div className="flex items-center gap-2">
+          <div
+            className="pointer-events-auto flex h-full cursor-pointer items-center justify-center rounded-lg border-2 border-purple-800/30 bg-gradient-to-tr from-gray-800/50 from-20% to-purple-700/30 to-90% p-3 text-2xl transition-colors duration-300 hover:bg-gradient-to-tr hover:from-gray-900/70 hover:to-purple-700/50"
+            onClick={toggleObstaclesEnabled}
+          >
+            {isObstaclesEnabled ? <FaRoad /> : <FaExplosion />}
+          </div>
+          <div>Toggle Obstacles</div>
         </div>
       </div>
     </div>
